00152a974fc692ade4099fbf60ceb951ed02cca5
Justin <42159955+Justin-Luk@users.noreply.github.com>
2021-12-14 13:35:41 -0500

    Add files via upload

--- Ultrasound_Processer.m
+++ Ultrasound_Processer.m
@@ -1,6 +1,3 @@
-%A = imread('copy.jpg');
-%imshow(A)
-
 %First, outline the area of calibration in imagej and use the histogram to
 %find the mean. Outline the area of interest in imagej and find the mean
 %the same way. Subtract mean of area of calibration from area of interest
@@ -11,9 +8,12 @@ rawIMG = imread('copy.jpg');
 medIMG = medfilt2(rawIMG(:,:,2), [10,10]);  
 
 % Make contour plot
-v = [ 37.688 37.688]
-contourf(medIMG(:,:,1),v)
-%Area=sum(contourf(medIMG(:,:,1),v)>1,'all')
-set(gca,'xdir','normal','ydir','reverse')
+v = [ 37.688 37.688]; %height of interest
+contourf(medIMG(:,:,1),v) % Cross section of contour plot 
+set(gca,'xdir','normal','ydir','reverse') % Flip to orient it correctly 
 
-Area1=sum(medIMG(:,:,1)==38,'all')
+%The math
+Area1=sum(medIMG(:,:,1)==38,'all') % number of pixels at z=38 
+B = 338*482; % Number of pixels in the picture
+Area2 = Area1 / B;  % Gives a percentage of the image that is colored
+Percent_Colored = Area2*100 
