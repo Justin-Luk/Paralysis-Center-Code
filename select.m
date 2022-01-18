20e0ca377285120bb3d91b635c145eb5cd8dbb41
Justin <42159955+Justin-Luk@users.noreply.github.com>
2022-01-18 15:00:53 -0500

    Add files via upload

---
+++ select.m
@@ -0,0 +1,113 @@
+% Demo to have the user freehand draw an irregular shape over a color image.
+% Then it creates a new image where the drawn region is all green inside the region and untouched outside the region,
+
+% Change the current folder to the folder of this m-file.
+
+
+if(~isdeployed)
+  cd(fileparts(which(mfilename)));
+end
+%clc;  % Clear command window.
+clear;  % Delete all variables.
+close all;  % Close all figure windows except those created by imtool.
+imtool close all;  % Close all figure windows created by imtool.
+workspace;  % Make sure the workspace panel is showing.
+fontSize = 16;
+
+% Read in a standard MATLAB gray scale demo image.
+folder = fileparts(which('11.38.44 hrs __[0003131]copy.jpg')); % Determine where demo folder is (works with all versions).
+baseFileName = '11.38.44 hrs __[0003131]copy.jpg ';
+
+baseFileNameNOext = baseFileName(1:(end-4));
+
+% Get the full filename, with path prepended.
+fullFileName = fullfile(folder, baseFileName);
+% Check if file exists.
+if ~exist(fullFileName, 'file')
+  % File doesn't exist -- didn't find it there.  Check the search path for it.
+  fullFileName = baseFileName; % No path this time.
+  if ~exist(fullFileName, 'file')
+    % Still didn't find it.  Alert user.
+    errorMessage = sprintf('Error: %s does not exist in the search path folders.', fullFileName);
+    uiwait(warndlg(errorMessage));
+    return;
+  end
+end
+rgbImage = imread(fullFileName);
+
+output_folder = 'C:\Users\justi\OneDrive\Documents\MATLAB\EMG';
+
+iter = 0;
+while(1)
+    iter = iter + 1;
+
+    imshow(rgbImage, []);
+    axis on;
+    title('Original Color Image', 'FontSize', fontSize);
+    set(gcf, 'Position', get(0,'Screensize')); % Maximize figure.
+    
+    % Ask user to draw freehand mask.
+    %message = sprintf('Left click and hold to begin drawing.\nSimply lift the mouse button to finish');
+    %uiwait(msgbox(message));
+    hFH = imfreehand(); % Actual line of code to do the drawing.
+    % Create a binary image ("mask") from the ROI object.
+    binaryImage = hFH.createMask();
+    xy = hFH.getPosition;
+
+    csvFileName = [baseFileNameNOext '_Mask' num2str(iter) '_xy.csv'];
+    csvwrite([output_folder '\' csvFileName],xy)
+    
+    % Now make it smaller so we can show more images.
+    subplot(2, 2, 1);
+    imshow(rgbImage, []);
+    axis on;
+    drawnow;
+    title('Original gray scale image', 'FontSize', fontSize);
+    
+    % Display the freehand mask.
+    subplot(2, 2, 2);
+    imshow(binaryImage);
+    axis on;
+    title('Binary mask of the region', 'FontSize', fontSize);
+    
+    % Get coordinates of the boundary of the freehand drawn region.
+    structBoundaries = bwboundaries(binaryImage);
+    xy=structBoundaries{1}; % Get n by 2 array of x,y coordinates.
+    x = xy(:, 2); % Columns.
+    y = xy(:, 1); % Rows.
+    subplot(2, 2, 1); % Plot over original image.
+    hold on; % Don't blow away the image.
+    plot(x, y,'k', 'LineWidth', 2);
+    drawnow; % Force it to draw immediately.
+   
+    % Extract the individual red, green, and blue color channels.
+    redChannel = rgbImage(:, :, 1);
+    greenChannel = rgbImage(:, :, 2);
+    blueChannel = rgbImage(:, :, 3);
+
+    %meanval = mean2(maskedRgbImage)
+
+    % Assign colors within each color channel individually.
+    redChannel(~binaryImage) = 255;
+    greenChannel(~binaryImage) = 255;
+    blueChannel(~binaryImage) = 255;
+    % Recombine separate masked color channels into a single, true color RGB image.
+    maskedRgbImage = cat(3, redChannel, greenChannel, blueChannel);
+    
+    burnedImage(~binaryImage) = 255;
+    % Display the image with the mask "burned in."
+    subplot(2, 2, 3);
+    imshow(maskedRgbImage);
+
+    maskFileName = [baseFileNameNOext '_Mask' num2str(iter) '.bmp'];
+    imwrite(maskedRgbImage,[output_folder '\' maskFileName], 'bmp')
+    axis on;
+    caption = sprintf('Masked white outside the region');
+    title(caption, 'FontSize', fontSize);
+    
+    choice = menu('Press yes no','Yes','No');
+    if choice==2 | choice==0
+       break;
+    end
+end
+%close all;
